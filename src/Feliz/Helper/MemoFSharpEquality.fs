9226c9dd996a74bbd5fad62fe05e1561016ea24f
Kevin F <Freymaurer@gmx.de>
2025-11-27 11:50:10 +0100

    delete unused file

--- src/Feliz/Helper/MemoFSharpEquality.fs
+++
@@ -1,5 +0,0 @@
-// This module provides a function used by Feliz compilerplugins to perform basic fsharp equality checks. It should not be easy discoverable by end users.
-module Feliz.MemoFSharpEquality
-
-let GenericFSharpEqualFn<'T when 'T : equality> (prevProps: 'T) (nextProps: 'T) =
-    prevProps = nextProps
